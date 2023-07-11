a1853adf11eb498ef070acf8b3fcb71e7852c7f1
sdfgx123 <sdfgx123@naver.com>
2023-07-11 14:10:46 +0900

    init.

---
+++ src/main/java/com/fastcampus/toy3/Toy3Application.java
@@ -0,0 +1,13 @@
+package com.fastcampus.toy3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toy3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toy3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcampus/toy3/Toy3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.toy3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toy3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
