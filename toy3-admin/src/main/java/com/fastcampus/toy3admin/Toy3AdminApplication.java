4e4e114040d2c0a7333c6783d7edc16d976a4c60
sdfgx123 <sdfgx123@naver.com>
2023-07-11 16:57:33 +0900

    admin module init.

---
+++ toy3-admin/src/main/java/com/fastcampus/toy3admin/Toy3AdminApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.toy3admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toy3AdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toy3AdminApplication.class, args);
+	}
+
+}
---
+++ toy3-admin/src/test/java/com/fastcampus/toy3admin/Toy3AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.toy3admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toy3AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
