9468a2ecf1b764ba487117df84924ba9ec7f63e4
Joonhee Jeong <jeonggoo75@gmail.com>
2023-07-13 15:57:44 +0900

    modify test #4 - save newbie, vip
    각 카테고리별 게시글 추가 테스트 진행

--- src/test/java/com/fastcampus/toy3/domain/post/repository/PostJpaRepositoryTest.java
+++ src/test/java/com/fastcampus/toy3/domain/post/repository/PostJpaRepositoryTest.java
@@ -18,19 +18,35 @@ class PostJpaRepositoryTest {
     private PostRepository postRepository;
 
     @Test
-    @DisplayName("게시글 추가")
-    void save() {
-        Post post = Post.builder()
-                .category(Category.NEWBIE)
-                .title("첫 게시글 입니다.")
-                .content("안녕하세요, 반갑습니다. 초심자입니다.")
-                .userNickname("초심자")
-                .build();
+    @DisplayName("게시글 추가 - 새싹")
+    void saveNewbie() {
+        Post post = buildPost(Category.NEWBIE);
 
         Post savedPost = postRepository.save(post);
 
         assertThat(savedPost).isSameAs(post);
         assertThat(savedPost.getId()).isNotNull();
+        assertThat(savedPost.getCategory()).isEqualTo(Category.NEWBIE);
     }
 
+    @Test
+    @DisplayName("게시글 추가 - 우수")
+    void saveVip() {
+        Post post = buildPost(Category.VIP);
+
+        Post savedPost = postRepository.save(post);
+
+        assertThat(savedPost).isSameAs(post);
+        assertThat(savedPost.getId()).isNotNull();
+        assertThat(savedPost.getCategory()).isEqualTo(Category.VIP);
+    }
+
+    private Post buildPost(Category category) {
+        return Post.builder()
+                .category(category)
+                .title("첫 게시글 입니다.")
+                .content("안녕하세요, 반갑습니다. 초심자입니다.")
+                .userNickname("초심자")
+                .build();
+    }
 }
