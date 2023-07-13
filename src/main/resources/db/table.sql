410bf6b40fe3f0250127abf454c25efb95cf0ad4
Joonhee Jeong <jeonggoo75@gmail.com>
2023-07-13 15:41:25 +0900

    modify #1 - type of category

--- src/main/resources/db/table.sql
+++ src/main/resources/db/table.sql
@@ -2,7 +2,7 @@ drop table if exists post;
 
 create table post(
     id bigint auto_increment,
-    category char(2) not null,
+    category varchar(2) not null,
     title varchar(30) not null,
     content text(2000),
     thumbnail mediumblob,
@@ -13,3 +13,6 @@ create table post(
 );
 
 create index post_search_idx on post (user_nickname, title);
+
+select default_character_set_name, default_collation_name FROM information_schema.schemata
+where schema_name = 'fastcampus_toy3';
