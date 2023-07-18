3218efa6754b2271e93a7ade64463235d0e4130d
Joonhee Jeong <jeonggoo75@gmail.com>
2023-07-18 17:08:49 +0900

    implement #7 - save of PostService

---
+++ src/main/java/com/fastcampus/toy3/domain/post/service/PostService.java
@@ -0,0 +1,33 @@
+package com.fastcampus.toy3.domain.post.service;
+
+import com.fastcampus.toy3.domain.post.Post;
+import com.fastcampus.toy3.domain.post.dto.PostWriteForm;
+import com.fastcampus.toy3.domain.post.file.FileManager;
+import com.fastcampus.toy3.domain.post.file.UploadedFile;
+import com.fastcampus.toy3.domain.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+    private final FileManager fileManager;
+
+    @Transactional
+    public Long save(PostWriteForm form) throws IOException {
+        UploadedFile uploadedFile = fileManager.storeFile(form.getThumbnail());
+
+        Post post = postRepository.save(
+                new Post(form.getTitle(),
+                        form.getContent(),
+                        uploadedFile.getUploadedName(),
+                        uploadedFile.getStoredName()));
+
+        return post.getId();
+    }
+}
